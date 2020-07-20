5f3f776e6337e42511c99aa07bb4d6ccd34685ea
Adithya Nair <mail@adtya.xyz>
2020-07-21 00:39:25 +0530

    add model for chatrooms

---
+++ models/chat-model.js
@@ -0,0 +1,19 @@
+const mongoose = require("../config/db");
+const Schema = mongoose.Schema;
+
+//schema for storing chat details
+const chatRoomSchema = new Schema({
+  source: { type: String, required: true },
+  sink: { type: String, required: true },
+  thread: [
+    {
+      from: { type: String, required: true },
+      sentTime: { type: Date, required: true },
+      content: { type: String, required: true },
+    },
+  ],
+});
+
+var ChatRoom = mongoose.model("chatroom", chatRoomSchema);
+
+module.exports = ChatRoom;
