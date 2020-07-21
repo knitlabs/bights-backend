9f15a7b563b7f5122296af9aeaf414e58de4c85e
Adithya Nair <mail@adtya.xyz>
2020-07-21 11:49:41 +0530

    turn off MongoDB's findAndModify() for Mongoose's findByIdAndUpdate()

--- config/db.js
+++ config/db.js
@@ -4,7 +4,11 @@ dbURI = process.env.DB_URI;
 if (typeof dbURI !== "undefined") {
   mongoose.connect(
     dbURI,
-    { useNewUrlParser: true, useUnifiedTopology: true },
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    },
     (err) => {
       if (err) console.log(err);
       else console.log("Database connected");
